4c6ed2e43e92521fd2de4e675bb656c82d65cb60
Clayton Bauman <claytonkb@gmail.com>
2018-10-10 15:53:39 -0700

    Added make.pl

---
+++ make.pl
@@ -0,0 +1,87 @@
+# make.pl
+#
+# Use this to build zebra on any standard *nix with GCC
+#
+# Use at your own risk. I use Perl instead of autotools because the latter
+#    are an opaque morass of Mad Hatter gibberish.
+#
+#   USAGE:
+#
+#   Build all:
+#       perl make.pl
+#
+#   Clean:
+#       perl make.pl clean
+
+unless ($#ARGV > -1) {
+    make_all();
+}
+else{
+    if($ARGV[0] eq "clean"){
+        clean();
+    }
+#    elsif($ARGV[0] eq "clean_all"){
+#        clean_all();
+#    }
+#    elsif($ARGV[0] eq "lib_babel"){
+#        lib_babel();
+#    }
+#    elsif($ARGV[0] eq "all_libs"){
+#        all_libs();
+#    }
+    elsif($ARGV[0] eq "libs"){
+        libs();
+    }
+    elsif($ARGV[0] eq "all"){
+        make_all();
+    }
+}
+
+sub make_all{
+    libs();
+    build();
+}
+
+#sub all_libs{
+#    lib_babel();
+#    libs();
+#    `cp lib_babel/obj/* obj/`;
+#}
+#
+#sub lib_babel{
+#    chdir "lib_babel";
+#    `perl make.pl libs`;
+#    chdir "../";
+#}
+
+sub libs{
+    `mkdir -p obj`;
+    chdir "src";
+    `g++ -c *.cpp -std=c++11`;
+    `mv *.o ../obj`;
+    chdir "../";
+}
+
+sub build{
+    `mkdir -p bin`;
+    my @objs = `ls obj`;
+    my $obj_string = "";
+    for(@objs){ chomp $_; $obj_string .= "obj/$_ " };
+    my $build_string =
+        "g++ -std=c++11 test/main.cpp $obj_string -Isrc -o bin/test";
+    `$build_string`;
+}
+
+#sub clean_all{
+#    chdir "lib_babel";
+#    `perl make.pl clean`;
+#    chdir "../";
+#    clean();
+#}
+
+sub clean{
+    `rm -rf obj`;
+    `rm -rf bin`;
+}
+
+
